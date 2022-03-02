ea51664cf1894e16731154a65cb228f52d0df619
SKR301 <chunnu.bihat@gmail.com>
2022-03-03 00:14:03 +0530

    #3 added parentzip or unique zip feature

--- main.py
+++ main.py
@@ -6,21 +6,32 @@ def optimizeDir(dir):
 
 def writeDirInFile(dir):
     cmd = "dir "+dir+"/b>dir.txt"
-    os.system(cmd)
+    x = os.system(cmd)
 
 def readToTuple():
     dirFile = open("dir.txt","r")
     folders = dirFile.read()
     return tuple(map(str, folders.split('\n')))
 
-def zipIt(folderList):
+def zipItUnique(folderList):
     for folderName in folderList:
         if(folderName == ''):
             continue
         shutil.make_archive(dir+"\\"+folderName, "zip", dir+"\\"+folderName)
 
+def zipItParent(dir):
+    shutil.make_archive(dir, "zip", dir)
+
 if __name__=="__main__":
     dir = str(input("Enter complete directory to folders:"))
-    writeDirInFile("\""+dir+"\"")
-    folderList = readToTuple()
-    zipIt(folderList)
+    type = str(input("'Unique' or 'Parent':"))
+
+    if type in ('Parent','PARENT','parent','p','P'):
+        zipItParent(dir)
+        print("Creating a Single Zip file for "+dir)
+
+    if type in ('Unique','UNIQUE','unique','UNI','Uni','uni','u','U'):
+        writeDirInFile("\""+dir+"\"")
+        folderList = readToTuple()
+        zipItUnique(folderList)
+        print("Creating Unique Zip files for each folder inside "+dir)
