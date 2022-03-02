ca96d94bca215454013c86f727a86f3c6667cb2c
SKR301 <chunnu.bihat@gmail.com>
2022-03-02 23:26:27 +0530

    read from dir.txt

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ def writeFolders():
 
 if __name__=="__main__":
     writeFolders()
+
+    dirFile = open("dir.txt","r")
+    folders = dirFile.read()
+    folder = tuple(map(str, folders.split('\n')))
+
+    
+
+    # shutil.make_archive("simonsZip", "zip", "files")
