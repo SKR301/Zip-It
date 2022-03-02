38497302020f7bcf5d2087f3afe86dc03364405b
SKR301 <chunnu.bihat@gmail.com>
2022-03-02 23:40:47 +0530

    parent folder zip creation removed

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import shutil
 import os
 
-dir = "D:\\NewFolder"
+dir = "D:\\NewFolder\\"
 
 def writeFolders():
     cmd = "dir "+dir+"/b>dir.txt"
     os.system(cmd)
 
+
 if __name__=="__main__":
+    # dir = input("Enter complete directory to folders:")
+    # optimizeDir()
     writeFolders()
 
     dirFile = open("dir.txt","r")
@@ -15,4 +18,6 @@ if __name__=="__main__":
     folderList = tuple(map(str, folders.split('\n')))
 
     for folderName in folderList:
-        shutil.make_archive(dir+"\\"+folderName, "zip", dir+"\\"+folderName)
+        if(folderName == ''):
+            continue
+        shutil.make_archive(dir+folderName, "zip", dir+folderName)
